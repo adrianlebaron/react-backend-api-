62a61b929b9f3f12dde79e5aa27d87fba3ae980e
A.D <adrianlebaron@gmail.com>
2018-12-11 16:06:15 -0700

    fixed some syntax and routes

--- app.py
+++ app.py
@@ -5,28 +5,46 @@ from flask_heroku import Heroku
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = ""
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://astkckyygpynfq:3798e18db984cbd387d0231dbe399ef2f868d1038510c2e7a8a624bc810a4488@ec2-23-21-188-236.compute-1.amazonaws.com:5432/dei5te2i5in00k"
 
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 
-class Movie(db.Model)
-  __tablename__ = "movies"
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(120))
-  rating = db.column(db.Integer)
+class Movie(db.Model):
+	__tablename__ = "movies"
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(120))
+	rating = db.Column(db.Integer)
 
-  def __init__(self, title, rating);
-    self.title = title
-    self.rating = rating
+	def __init__(self, title, rating):
+		self.title = title
+		self.rating = rating
 
-  def __repr__(self)
-    return '<Title %r>' % self.title
+	def __repr__(self):
+		return '<Title %r>' % self.title
+
 
 @app.route('/')
-def home ():
-  return "<h1>Hello!</h1>"
+def home():
+	return "<h1>Hello!</h1>"
+
+@app.route('/movies/input', methods=['POST'])
+def movies_input():
+	if request.content_type == 'application/json':
+		post_data = request.get_json()
+		title = post_data.get('title')
+		rating = post_data.get('rating')
+		reg = Movie(title, rating)
+		db.session.add(reg)
+		db.session.commit()
+		return "Data Posted"
+	return ""
+
+@app.route('/return/movies', methods=['GET'])
+def return_movies():
+	all_movies = db.session.query(Movie.title, Movie.rating).all()
+	return jsonify(all_movies)
 
 if __name__ == '__main__':
-  app.debug = True
-  app.run()
+	app.debug = True
+	app.run()
